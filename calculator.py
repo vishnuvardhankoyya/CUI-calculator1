c1c5c67c2517c39023c811144690a0c2d57376c2
tejavarddhank <kodalitejavarddhan@gmail.com>
2023-03-01 17:41:19 +0530

    Code inserted divison

--- calculator.py
+++ calculator.py
@@ -15,3 +15,22 @@ if select == '1':
     
 else: 
     print("Invalid input!")
+
+def divide(number1, number2): 
+    return (number1 / number2)
+
+print("Operation Menu -\n" \  
+        "4. Division of two numbers\n")
+
+select = input("Select operations form 1, 2, 3, 4 :") 
+  
+num1 = int(input("Enter first number: ")) 
+num2 = int(input("Enter second number: ")) 
+
+if select == '4': 
+    print(num1, "/", num2, "=", 
+                    divide(num1, num2)) 
+else: 
+    print("Invalid input!")
+
+
