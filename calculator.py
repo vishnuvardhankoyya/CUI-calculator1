f7b643480cc190c49f4a6fced45dd50099d31eaf
vishnuvardhankoyya <koyya.vardhan20@st.niituniversity.in>
2023-03-01 17:52:20 +0530

    modifed the final code

--- calculator.py
+++ calculator.py
@@ -1,75 +1,40 @@
-def add(number1, number2): 
-    return (number1 + number2) 
+def add(num1, num2):
+    return num1 + num2
 
-print("Operation Menu -\n" \ 
-        "1. Addition of two numbers\n" \ )
+def subtract(num1, num2):
+    return num1 - num2
 
-select = input("Select operations form 1, 2, 3, 4 :") 
-  
-num1 = int(input("Enter first number: ")) 
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+# Take input from the user
+choice = input("Enter choice (1/2/3/4): ")
+
+num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
-if select == '1': 
-    print(num1, "+", numb2, "=", 
-                    add(num1, num2)) 
-    
-else: 
-    print("Invalid input!")
+if choice == '1':
+    print(num1, "+", num2, "=", add(num1,num2))
 
-def divide(number1, number2): 
-    return (number1 / number2)
+elif choice == '2':
+    print(num1, "-", num2, "=", subtract(num1,num2))
 
-print("Operation Menu -\n" \  
-        "4. Division of two numbers\n")
+elif choice == '3':
+    print(num1, "*", num2, "=", multiply(num1,num2))
 
-select = input("Select operations form 1, 2, 3, 4 :") 
-  
-num1 = int(input("Enter first number: ")) 
-num2 = int(input("Enter second number: ")) 
-
-if select == '4': 
-    print(num1, "/", num2, "=", 
-                    divide(num1, num2)) 
-else: 
-    print("Invalid input!")
-
-def subract(number1, number2): 
-    return (number1 + number2) 
-
-print("Operation Menu -\n" \ 
-        "2. subtraction of two numbers\n" \ )
-
-select = input("Select operations form 1, 2, 3, 4 :") 
-  
-num1 = int(input("Enter first number: ")) 
-num2 = int(input("Enter second number: "))
-
-if select == '2': 
-    print(num1, "-", num2, "=", 
-                    subtract(num1, num2)) 
-    
-else: 
-    print("Invalid input!")
-
-def multiply(number1, number2): 
-    return (number1 * number2) 
-  
-
-  
-print("Operation Menu -\n" \ 
-        "3. Multiplication of two numbers\n" ) 
-  
-  
-#Taking input from the user  
-select = input("Select operations form 1, 2, 3, 4 :") 
-  
-num1 = int(input("Enter first number: ")) 
-num2 = int(input("Enter second number: ")) 
-
-  
-if select == '3': 
-    print(num1, "*", num2, "=", 
-                    multiply(num1, num2)) 
- 
-else: 
-    print("Invalid input!")
+elif choice == '4':
+    if num2 == 0:
+        print("undefined")
+    else:
+        print(num1, "/", num2, "=", divide(num1,num2))
+else:
+    print("Invalid input")
