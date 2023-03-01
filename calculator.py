c6cd6526c369bd6c23aa4bb02147e377ac8ab0fb
vishnuvardhankoyya <koyya.vardhan20@st.niituniversity.in>
2023-03-01 17:08:00 +0530

    addition code added

--- calculator.py
+++ calculator.py
@@ -1,1 +1,17 @@
+def add(number1, number2): 
+    return (number1 + number2) 
 
+print("Operation Menu -\n" \ 
+        "1. Addition of two numbers\n" \ )
+
+select = input("Select operations form 1, 2, 3, 4 :") 
+  
+num1 = int(input("Enter first number: ")) 
+num2 = int(input("Enter second number: "))
+
+if select == '1': 
+    print(num1, "+", numb2, "=", 
+                    add(num1, num2)) 
+    
+else: 
+    print("Invalid input!")
